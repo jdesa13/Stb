4301c0d1b2d2e08370e21205676e6a44493e95ac
hepcatjk <kiderj@gmail.com>
2018-05-09 14:45:54 -0400

    JTK: Pixel color loop to make colorful images.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,50 @@
 #include <iostream>
 #include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#define STBI_MSC_SECURE_CRT
+#include <stb_image.h>
 #include <stb_image_write.h>
 
-
+#define BITMAPRANGE 255
 
 int main(int argc, char *argv[]) {
-	
-	int width = 1024;
-	int height = 728;
-	const char* filename = "A.png";
-	int size = width * height * 4;
-	
-	char* img =(char*)malloc(size);
-	if (img == NULL) {
 
-		std::cout << "FAILED" << std::endl;
+	std::string filename = "A.png";
+	
+	unsigned int mWidth = 1024;
+	unsigned int mHeight = 728;
+	unsigned int size = mWidth * mHeight * 4;
+	
+	//char* img =(char*)malloc(size); //JTK
+
+	//Scale and cast for bitmap color range.
+	uint8_t *pixels = new uint8_t[size];
+
+
+
+	if (pixels == NULL) {
+		std::cout << "FAILED: Falled to allocate memory for Image!" << std::endl;
 		return 0;
-
 	}
 
-	stbi_write_png(filename,width,height,4,img,width*4);
 
-	free(img);
+	unsigned int index = 0;
+	for (int rows =0; rows<mHeight; rows++)
+		for (int cols = 0; cols < mWidth; cols++)
+		{
+			index = rows*mWidth * 4 + cols * 4;
+			pixels[index + 0] = BITMAPRANGE * 0; //Red
+			pixels[index + 1] = BITMAPRANGE * 1; //Green
+			pixels[index + 2] = BITMAPRANGE * 0; //Blue
+			pixels[index + 3] = BITMAPRANGE * 1; // Alpha
+		}
+
+
+
+
+	stbi_write_png(filename.c_str(), mWidth, mHeight, 4, pixels, mWidth*4);
+
+	free(pixels);
 
 	return 0;
 }
